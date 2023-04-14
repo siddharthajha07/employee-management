03d3c21cd24536ef563498847ab90b0eb2de4fec
Siddhartha Jha <siddharthajha07@gmail.com>
2023-04-13 20:37:20 -0400

    Employee Management - Initial Commit

---
+++ employee-management-system/employee-system-api/src/main/java/com/dailycodebuffer/Employee/services/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.dailycodebuffer.Employee.services;
+
+import com.dailycodebuffer.Employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
